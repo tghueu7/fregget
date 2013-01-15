321ca6838c030473184ed0a0e7200a62e6b30ecc
Andrea Aime <andrea.aime@gmail.com>
2013-01-15 15:11:50 +0100

    Stop this test from accessing the internet while we have all the resources it needs on the local disk

--- modules/extension/xsd/xsd-gml3/src/test/java/org/geotools/gml3/v3_2/EmfXsdLoadTest.java
+++ modules/extension/xsd/xsd-gml3/src/test/java/org/geotools/gml3/v3_2/EmfXsdLoadTest.java
@@ -17,17 +17,24 @@
 
 package org.geotools.gml3.v3_2;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.namespace.QName;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.URIHandlerImpl;
 import org.eclipse.xsd.XSDElementDeclaration;
 import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.XSDTypeDefinition;
 import org.eclipse.xsd.impl.XSDSchemaImpl;
 import org.eclipse.xsd.util.XSDResourceImpl;
+import org.geotools.data.DataUtilities;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.SchemaIndex;
 import org.geotools.xml.Schemas;
@@ -106,8 +113,32 @@ public class EmfXsdLoadTest {
     @SuppressWarnings("unchecked")
     private static void checkChildElementTypes(String filename, String namespace, String name) {
         ResourceSet resourceSet = XSDSchemaImpl.createResourceSet();
-        XSDResourceImpl resource = (XSDResourceImpl) resourceSet.getResource(
-                URI.createURI(EmfXsdLoadTest.class.getResource(filename).toString()), true);
+        resourceSet
+                .getURIConverter()
+                .getURIHandlers()
+                .add(0,
+                        new LocalURIHanlder("http://schemas.opengis.net/iso/19139/20070417",
+                                "file://./src/main/resources/org/geotools/gml3/v3_2"));
+        resourceSet
+                .getURIConverter()
+                .getURIHandlers()
+                .add(0,
+                        new LocalURIHanlder("http://schemas.opengis.net/gml/3.2.1",
+                                "file://./src/main/resources/org/geotools/gml3/v3_2"));
+        resourceSet
+                .getURIConverter()
+                .getURIHandlers()
+                .add(0,
+                        new LocalURIHanlder("http://www.w3.org/1999/xlink.xsd",
+                                "file://../xsd-core/src/main/resources/org/geotools/xlink/xlink.xsd"));
+        resourceSet
+                .getURIConverter()
+                .getURIHandlers()
+                .add(0,
+                        new LocalURIHanlder("http://www.w3.org/2001/xml.xsd",
+                                "file://../xsd-core/src/main/resources/org/geotools/xml/xml.xsd"));
+        URI resourceLocation = URI.createURI(EmfXsdLoadTest.class.getResource(filename).toString());
+        XSDResourceImpl resource = (XSDResourceImpl) resourceSet.getResource(resourceLocation, true);
         XSDSchema schema = resource.getSchema();
         Assert.assertNotNull(schema);
         SchemaIndex index = null;
@@ -134,6 +165,43 @@ public class EmfXsdLoadTest {
         }
         System.err.println();
         Assert.assertFalse("Unexpected child element declaration with null type", foundNull);
+    }
+    
+    private static class LocalURIHanlder extends URIHandlerImpl {
+        
+        String remotePrefix;
+        String localPrefix;
+        
+        public LocalURIHanlder(String remotePrefix, String localPrefix) {
+            super();
+            this.remotePrefix = remotePrefix;
+            this.localPrefix = localPrefix;
+        }
+
+        @Override
+        public boolean canHandle(URI uri) {
+            // System.out.println("Looking into " + uri);
+
+            if(uri.isFile()) {
+                return false;
+            }
+            String location = uri.toString();
+            if(location.startsWith(remotePrefix)) {
+                return true;
+            }
+            return false;
+        };
+        
+        @Override
+        public InputStream createInputStream(URI uri, Map<?, ?> options) throws IOException {
+            String updated = uri.toString().replace(remotePrefix, localPrefix);
+            File canonical = DataUtilities.urlToFile(new URL(updated)).getCanonicalFile();
+            if(!canonical.exists()) {
+                throw new RuntimeException("Failed to locate file on the local filesystem: " + canonical.getAbsolutePath());
+            }
+            // System.out.println("Handled " + uri + " into " + canonical.getAbsolutePath());
+            return super.createInputStream(URI.createFileURI(canonical.getAbsolutePath()), options);
+        }
     }
 
 }
