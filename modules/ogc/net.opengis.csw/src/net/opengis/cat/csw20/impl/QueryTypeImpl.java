38260eea202e3927d7d04540889c00eabe0cf484
Daniel Cruver <ddcruver@gmail.com>
2020-05-05 21:00:09 -0400

    Revert unrelated code change in generated code.

--- modules/ogc/net.opengis.csw/src/net/opengis/cat/csw20/impl/QueryTypeImpl.java
+++ modules/ogc/net.opengis.csw/src/net/opengis/cat/csw20/impl/QueryTypeImpl.java
@@ -6,7 +6,6 @@
  */
 package net.opengis.cat.csw20.impl;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -375,7 +374,7 @@ public class QueryTypeImpl extends AbstractQueryTypeImpl implements QueryType {
             case Csw20Package.QUERY_TYPE__CONSTRAINT:
                 return constraint != null;
             case Csw20Package.QUERY_TYPE__SORT_BY:
-                return SORT_BY_EDEFAULT == null ? sortBy != null : !Arrays.equals(SORT_BY_EDEFAULT, sortBy);
+                return SORT_BY_EDEFAULT == null ? sortBy != null : !SORT_BY_EDEFAULT.equals(sortBy);
             case Csw20Package.QUERY_TYPE__TYPE_NAMES:
                 return typeNames != null;
         }
